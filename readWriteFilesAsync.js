7dad9ff06f798b6c984feaae51c59a9a4ee2ca59
akshaykalaskar <akshay.kalaskar@hotmail.com>
2025-12-06 23:08:54 +0000

    Async way

---
+++ readWriteFilesAsync.js
@@ -0,0 +1,15 @@
+// File Read/ Write
+const fs = require("fs");
+// Async way..
+// Reading file
+fs.readFile("./txt/input.txt", "utf-8", (err, data) => {
+  if (err) return console.log("Error reading file:", err);
+
+  // Writing now
+  const textOut = `This is new content. And this is existing content - ${data} \nCreated on ${Date.now()} today`;
+  fs.writeFile("./txt/output.txt", textOut, (err) => {
+    if (err) return console.log("Error writing file:", err);
+  });
+});
+
+console.log("File updated!");
